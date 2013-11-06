8dcdc64490b8cef31e7f2427725211ad05b2a61a
shoffing <shoffing@gmail.com>
2013-11-06 15:57:11 -0500

    Added mouse control ball thing to MainScene

---
+++ Torment/Assets/CursorController.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var ball : GameObject;
+
+function Start () {
+	ball = GameObject.Find("Cursor");
+}
+
+function Update () {
+
+	if(Input.GetMouseButton(0)) {
+		var mouseRay : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		var hit : RaycastHit;
+		if(Physics.Raycast(mouseRay, hit)) {
+			ball.rigidbody.AddForce( (hit.point - ball.transform.position) * 20 - ball.rigidbody.velocity * 2 );
+		}
+	}
+}
