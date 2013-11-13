89c175eeb1e414666bb134fd2644be22fa626871
LandsharkRAWR <yeoldehause@gmail.com>
2013-11-13 14:05:41 -0400

    the spinning box? it spins faster now

--- Torment/Assets/scripts/RoamingBox.js
+++ Torment/Assets/scripts/RoamingBox.js
@@ -6,5 +6,5 @@ function Start () {
 
 function Update () {
 	rigidbody.velocity = Vector3(-0.5,rigidbody.velocity.y,0);
-	rigidbody.angularVelocity = Vector3(0,-3,0);
+	rigidbody.angularVelocity = Vector3(0,-12,0);
 }
