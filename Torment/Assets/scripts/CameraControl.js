607df2127d5ced038ff32dabc75fc92b1d7b4fbd
shoffing <shoffing@gmail.com>
2013-11-12 19:41:13 -0500

    Added MovementTestbed scene

---
+++ Torment/Assets/scripts/CameraControl.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+private var player : GameObject;
+
+function Start () {
+	player = GameObject.FindGameObjectWithTag("Player");
+}
+
+function Update () {
+	transform.position = player.transform.position + Vector3(0, 10, -10);
+	transform.LookAt(player.transform.position);
+}
---
+++ Torment/Assets/scripts/PlayerControl.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+static var SPEED : float = 8;
+
+var groundPlane : Plane;
+
+function Start () {
+	groundPlane = Plane(Vector3(0, 1, 0), 0);
+}
+
+function Update () {
+	// Player rotation towards mouse cursor
+	var mouseRay : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	var hitDist : float;
+	
+	if(groundPlane.Raycast(mouseRay, hitDist)) {
+		var hitPoint : Vector3 = mouseRay.GetPoint(hitDist);
+		transform.LookAt(Vector3(hitPoint.x, transform.position.y, hitPoint.z));
+	}
+	
+	// Player movement
+	var inputVec = Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+	rigidbody.velocity = Vector3.Normalize(inputVec) * SPEED;
+}
