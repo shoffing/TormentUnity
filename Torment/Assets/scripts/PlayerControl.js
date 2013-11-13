ceb199a106da8e1907733b05a7eb61f2ca145c1a
shoffing <shoffing@gmail.com>
2013-11-13 01:22:02 -0500

    Cleaned up Oose's code
    Was kind of gross, able to condense a lot while still keeping it
    readable

--- Torment/Assets/scripts/PlayerControl.js
+++ Torment/Assets/scripts/PlayerControl.js
@@ -1,17 +1,22 @@
 ﻿#pragma strict
 
+// Static CONSTANTS
 static var SPEED : float = 1;
-static var JUMPSPEED : float = 5;
+static var JUMP_SPEED : float = 5;
 static var SLOPE_THRESHOLD : float = 0.7;
 
-var groundPlane : Plane;
+// Private Variables
+private var groundPlane : Plane;
 private var onGround = true;
 
-function Start () {
+// Public Variables
+// --
+
+function Start() {
 	groundPlane = Plane(Vector3(0, 1, 0), 0);
 }
 
-function Update () {
+function Update() {
 	// Player rotation towards mouse cursor
 	var mouseRay : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 	var hitDist : float;
@@ -20,33 +25,23 @@ function Update () {
 		var hitPoint : Vector3 = mouseRay.GetPoint(hitDist);
 		transform.LookAt(Vector3(hitPoint.x, transform.position.y, hitPoint.z));
 	}
-		
+	
 	// Player movement
-	var inputVec = Vector3(0,0,0);
-	if( onGround ){
-		inputVec = Vector3.Normalize( Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) );
-	}
-	else{
-		inputVec = Vector3.Normalize( Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) ) * 0.1;
-	}
+	var inputVec = Vector3.Normalize(Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"))) * (onGround ? 1 : 0.1);
 	
 	// Jump key
 	var jumpVec = Vector3(0,0,0);
 	if( Input.GetButtonDown("Jump") && onGround ){
-		jumpVec = Vector3(0, JUMPSPEED, 0);
+		jumpVec = Vector3(0, JUMP_SPEED, 0);
 		onGround = false;
 	}
 	
 	// Deceleration vector
-	var decelPercent = 0.1;
-	if( !onGround ){
-		decelPercent = 0.01;
-	}
-	var decelVec = Vector3(rigidbody.velocity.x,0,rigidbody.velocity.z) * decelPercent * -1 ;
+	var decelPercent = onGround ? 0.1 : 0.01;
+	var decelVec = Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z) * decelPercent * -1;
 	
 	// Setting player velocity
-	//rigidbody.velocity = stopVec + Vector3.Normalize(inputVec) * SPEED + jumpVec;
-	rigidbody.AddForce( inputVec * SPEED + decelVec + jumpVec , ForceMode.VelocityChange );
+	rigidbody.AddForce( inputVec * SPEED + decelVec + jumpVec, ForceMode.VelocityChange );
 
 }
 
@@ -58,5 +53,4 @@ function OnCollisionEnter(collision : Collision) {
 			onGround = true;
 		}
 	}
-	
 }
